af89b73c8a040bda1444150e173371b307224109
Anyanka Hopelessness <ahopelessness@gmail.com>
2019-05-06 18:43:13 +0300

    init

---
+++ verber/precache-manifest.69fe229cfc0d85065bb081172f8f1d9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cde320697fd78b81a7d4a6ad52e366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a5c407ab19e686769e5",
+    "url": "/static/css/main.54eb8ff7.chunk.css"
+  },
+  {
+    "revision": "98bfb5a9ef4a91e83a37",
+    "url": "/static/js/2.b1b8b76e.chunk.js"
+  },
+  {
+    "revision": "5a5c407ab19e686769e5",
+    "url": "/static/js/main.5c9d679b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
