f07aa08fb5a90635f996eb9889f8c34a994297d0
Anyanka Hopelessness <ahopelessness@gmail.com>
2019-05-06 18:46:41 +0300

    homepage change

---
+++ verber/precache-manifest.ff7850218c20818b8aad62704f5a27dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb440d78e34f6d126245ce77b6fe536",
+    "url": "/verber/index.html"
+  },
+  {
+    "revision": "e253349edf5c60940bc1",
+    "url": "/verber/static/css/main.54eb8ff7.chunk.css"
+  },
+  {
+    "revision": "98bfb5a9ef4a91e83a37",
+    "url": "/verber/static/js/2.b1b8b76e.chunk.js"
+  },
+  {
+    "revision": "e253349edf5c60940bc1",
+    "url": "/verber/static/js/main.470b4878.chunk.js"
+  },
+  {
+    "revision": "6642929adc591f8ca87b",
+    "url": "/verber/static/js/runtime~main.bb07e8aa.js"
+  }
+]);
